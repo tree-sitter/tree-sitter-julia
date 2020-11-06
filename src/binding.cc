777570ea61511405a67e2cc5181428569d250c30
Stephan Seitz <stephan.seitz@fau.de>
2020-11-06 23:53:22 +0100

    Fix typo: parametere -> parameter

--- src/binding.cc
+++ src/binding.cc
@@ -19,7 +19,7 @@ void Init(Local<Object> exports, Local<Object> module) {
   Local<Object> instance = constructor->NewInstance(Nan::GetCurrentContext()).ToLocalChecked();
   Nan::SetInternalFieldPointer(instance, 0, tree_sitter_julia());
 
-  Nan::Set(instance, Nan::New("name").ToLocalChecked(), Nan::New("python").ToLocalChecked());
+  Nan::Set(instance, Nan::New("name").ToLocalChecked(), Nan::New("julia").ToLocalChecked());
   Nan::Set(module, Nan::New("exports").ToLocalChecked(), instance);
 }
 
