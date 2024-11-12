9f2fb3b9a8c73ef0d2ec005c97f91274b492d48d
Sergio Alejandro Vargas <savargasqu+git@unal.edu.co>
2024-11-12 18:35:02 -0500

    Fix macro_identifier pattern

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -28,7 +28,9 @@
     (identifier) @function.call .))
 
 ; Macros
-(macro_identifier) @function.macro
+(macro_identifier
+  "@" @function.macro
+  (_) @function.macro)
 
 (macro_definition
   (signature
